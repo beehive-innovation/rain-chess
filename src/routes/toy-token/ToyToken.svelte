1affd08312ac7a8f0c49398963b94a2d2b292bdf
Mayur Nagare <mayurnagare@Mayurs-MacBook-Pro.local>
2022-10-11 19:02:40 +0530

    fixes & updation

--- src/routes/toy-token/ToyToken.svelte
+++ src/routes/toy-token/ToyToken.svelte
@@ -17,8 +17,12 @@
   import { getContext } from "svelte";
   import { push } from "svelte-spa-router";
   import Select from "$components/Select.svelte";
-  import { EmissionsERC20JSVM, type StateConfig } from "rain-sdk";
+  import {  EmissionsERC20JSVM, type StateConfig } from "rain-sdk"; 
+import {EmissionsERC20} from "rain-sdk" 
+import { ethers } from 'ethers';  
 
+  import AddressLibrary from "$routes/address-library/AddressLibrary.svelte";
+  import axios from 'axios'
   const { open } = getContext('simple-modal')
 
   let fields: any = {};
@@ -45,7 +49,8 @@
 
   // let parserVmStateConfig: Writable<StateConfig> = writable(null)
   let newEmissionsERC20
-  let simulatedResult
+  let simulatedResult 
+  let claimFlag
 
   $: if ($parserVmStateConfig && $signer) simulate()
 
@@ -67,9 +72,53 @@
     
   }
 
-  const handleClick = () =>{
-    console.log("demo");
+  const handleClick = async () =>{ 
+   
+    let id = tweetURL.split('/').pop() 
+  
+    let verifyReq
+    if(option.label.toLowerCase() == 'register'){
+      verifyReq = await axios.post('http://localhost:5000/api/v2/registerChessId' , {
+        tweetId :  id
+      })  
+      
+    }else if(option.label.toLowerCase() == 'verify game'){
+      let address = await $signer.getAddress() 
+       
+      verifyReq = await axios.post('http://localhost:5000/api/v2/chessGame' , {
+        tweetId :  id ,
+        address : address.toLowerCase()
+      })   
+    }  
+    console.log(verifyReq)
+    simulatedResult = `Claimable Tokens : ${verifyReq.data.data.gameTokens}` 
+    tweetURL = `` 
+    claimFlag = true
     
+  } 
+
+  const handleClick2 = async () => { 
+
+   
+ 
+    document.getElementById("express").style.display = "grid";
+      if(option.value == 1) document.getElementById("exp").style.display = "none";
+  } 
+
+  const claimCall = async () => {   
+    let address = await $signer.getAddress() 
+    let verifyContract = new EmissionsERC20('0x8fbf820107b88a54714ebe6debc26547ce31d914', $signer)   
+
+    let tx = await verifyContract.claim( address ,ethers.constants.AddressZero , {
+        gasPrice : ethers.utils.parseUnits('350', 'gwei'),
+        gasLimit :  '200000'
+    })  
+
+    let reuslt = await tx.wait()
+    console.log(reuslt )  
+
+     
+    claimFlag = false
   }
 
 </script>
@@ -81,27 +130,48 @@
     <span class="text-3xl font-semibold">Rain Chess</span>
 
     <div class="mb-2 flex flex-col w-full">
-      <Info>Create a token and your friends and can come and mint it. You set the rules of who can mint and how much they can mint. Here are some games you might want to play:</Info>
-      <div>example expressions...</div>
+      <Info>
+        <div class='row mb-2'>
+        <div class=' d-flex align-items-center'>
+          <img src='../../../public/assets/twitter.png' width='32' height='32' alt='twitter' class='me-4' />
+          <p>
+          Verify via Twitter, make a <a target='_blank' href='https://twitter.com/intent/tweet?text=Requesting%20verifcation%20of%20address%200x0000000000000000000000000000000000000000%20for%20%40rainprotocol.%20Check%20out%20Rain%20Rrotocol%20at%20https%3A%2F%2Fdocs.rainprotocol.xyz%2F%20%23rainprotocol%20%23nft'>tweet</a> with your Ethereum address pasted into the contents (surrounding text doesn't matter).<br />
+          Copy-paste the tweets URL into the above input box and fire away!
+          </p>
+      </div>
+      <div class=' d-flex align-items-center'>
+          <img src='../../../public/assets/lichess.svg' width='32' height='32' alt='twitter' class='me-4' />
+          <p>
+          Verify via Twitter, make a <a target='_blank' href='https://twitter.com/intent/tweet?text=Requesting%20verifcation%20of%20address%200x0000000000000000000000000000000000000000%20for%20%40rainprotocol.%20Check%20out%20Rain%20Rrotocol%20at%20https%3A%2F%2Fdocs.rainprotocol.xyz%2F%20%23rainprotocol%20%23nft'>tweet</a> with your Ethereum address pasted into the contents (surrounding text doesn't matter).<br />
+          Copy-paste the tweets URL into the above input box and fire away!
+          </p>
+      </div> 
+    </div>  
+
+
+      </Info>
+      <!-- <div>example expressions...</div> -->
     </div>
     <Section>
       <SectionHeading>Contract</SectionHeading>
       <SectionBody>
         <Item>
-          <Label>Name</Label>
-          <Info>Lorem Ipsum</Info>
+          <Label>Name</Label> 
+          <Info> 
+            <img src="../../../public/assets/knight2.png" class="h-6 w-6" />
+            Chess TKN</Info>
         </Item>
         <Item>
           <Label>Source</Label>
-          <Info>Lorem Ipsum</Info>
+          <Info>Rain</Info>
         </Item>
         <Item>
           <Label>Token</Label>
-          <Info>Lorem Ipsum</Info>
+          <Info>ERC20</Info>
         </Item>
         <Item>
           <Label>Contract details</Label>
-          <Info>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Info>
+          <Info>Mint a new ERC20 which releases new supply whenever a valid claim is made. We script the claim step, and each wallet trying to claim needs to be meet the criteria to mint their ERC20s. In this way claiming criteria can implicitly set a total supply cap, wallet supply cap and more. Can be used for game credits, project tokens, rewards.</Info>
         </Item>
       </SectionBody>
     </Section>
@@ -110,10 +180,11 @@
       <SectionHeading>Expressions (1)</SectionHeading>
       <SectionBody>
         <span class="text-xl font-semibold">Claimable amount expression</span>
-          <div class="max-w-prose">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+          <div class="max-w-prose">Enter the tweet URL and check claimable  <img src="../../../public/assets/knight2.png" class="h-6 w-6" /> ChessTKN</div>
           <div class="grid grid-cols-2 gap-4">
             <Input
               type="text"
+             
               placeholder="Tweet URL"
               bind:this={fields.tweetURL}
               bind:value={tweetURL}
@@ -123,7 +194,6 @@
             </Input>
             <Select
               items={Options}
-              bind:value={option}
               on:change={() => {
                   if(option.value != 2) document.getElementById("express").style.display = "grid";
                   else document.getElementById("express").style.display = "none";
@@ -133,6 +203,11 @@
             </Select>
           </div>
 
+          
+          <div class="flex flex-row gap-x-2 items-center  bg-violet-200 rounded-lg self-start p-3 max-w-prose">
+            <IconLibrary width={30} icon="tip" />
+            <div class="max-w-prose">Remember - You need energy to Register and Verify.</div>
+          </div>
           <div id="express" style="display: none;">
           <div class="grid grid-cols-7 gap-4 items-stretch" >
             <div class="col-span-4 flex flex-col gap-y-4">
@@ -141,7 +216,7 @@
             <div class="col-span-3">
               <div class="bg-amber-200 rounded-lg p-4 h-full">
 
-                <div class="font-mono text-black text-sm" >
+                <div class="font-mono text-black text-sm" >https://twitter.com/sid_bhoite/status/1578031489836781571
                   <span>Simulated output: </span>
                   <span>
                     {#if $signer}
@@ -158,8 +233,14 @@
               </div>
             </div>
           </div>
-          <div class="self-start flex flex-row items-center gap-x-2 py-4">
-            <Button shrink disabled={!$signer} on:click={handleClick}>Deploy EmissionsERC20</Button>
+          <div class="self-start flex flex-row items-center gap-x-2 py-4"> 
+
+            {#if !claimFlag}
+                <Button shrink disabled={!$signer} on:click={handleClick}>Submit</Button>
+            {:else}
+                <Button  shrink disabled={!$signer} on:click={claimCall}>Claim</Button>
+            {/if}
+            
             {#if !$signer}
             <span class="text-gray-600">Connect your wallet to deploy</span>
             {/if}
