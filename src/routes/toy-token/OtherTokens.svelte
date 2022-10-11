2bea2cefc18f5197ac85bf844407ae22e3ca4404
Mayur Nagare <mayurnagare@Mayurs-MacBook-Pro.local>
2022-10-11 19:05:46 +0530

    Code Fixes & updation

--- src/routes/toy-token/OtherTokens.svelte
+++ src/routes/toy-token/OtherTokens.svelte
@@ -1,11 +1,6 @@
 <script lang="ts">
-<<<<<<< HEAD
-	import { signerAddress, provider } from 'svelte-ethers-store'
-  import { Formatter } from 'rain-sdk';
-=======
 	import { signerAddress, signer, provider } from 'svelte-ethers-store'
   import { EmissionsERC20, Formatter } from 'rain-sdk';
->>>>>>> a632c8e (UI updation)
     import { onMount } from 'svelte';
     import { ethers } from 'ethers';
     import Item from './Item.svelte';
@@ -14,15 +9,6 @@
     import SectionBody from './SectionBody.svelte';
     import Section from './Section.svelte';
     import SectionHeading from './SectionHeading.svelte';
-<<<<<<< HEAD
-
-  let walletBalance = "";
-  onMount(async ()=>{
-    if($signerAddress) {
-      ethers
-    }
-  })
-=======
     import { VerifyConractDetails } from '$src/constants';
     import { getContractAddress } from 'ethers/lib/utils';
 
@@ -31,7 +17,6 @@
     let emissionsContract = ethers.utils.isAddress(VerifyConractDetails.contractAddress || "") ? new EmissionsERC20(VerifyConractDetails.contractAddress, $signer): null;
     walletBalance =  await emissionsContract.balanceOf($signerAddress)
   }
->>>>>>> a632c8e (UI updation)
 
   $: if($signerAddress && VerifyConractDetails.contractAddress) {
       getContract()
@@ -53,11 +38,7 @@
             <Item>
               <Label>Wallet Balance :</Label>
               <Info>
-<<<<<<< HEAD
-                {walletBalance}
-=======
                 {walletBalance?.toString()} Chess TKN
->>>>>>> a632c8e (UI updation)
               </Info>
             </Item>
           </SectionBody>
