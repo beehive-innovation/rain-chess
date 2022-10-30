675b5e7c345ea29f7fada7349699de9e8c9b6a24
Siddharth2207 <siddharthbhoite22@gmail.com>
2022-10-30 17:24:15 +0530

    Adding content

--- src/routes/rain-chess/LichessHome.svelte
+++ src/routes/rain-chess/LichessHome.svelte
@@ -20,11 +20,44 @@
         </div>
         <TabPanel>
             <div class="flex flex-col gap-y-4">
-                Welcome Home
-            </div>
+                Rain + LiChess : 
+
+                Play on LiChess and  collect rewards, game tokens,badges , nfts and much more ! 
+                Buy energy and mint assets based on your play. Trade your assets with other players,make your collection and win !  
+                ----------------------------------------------------------------------------------------------------------------------
+            </div>  
+         
+            <div class="flex flex-col gap-y-4">
+              
+                how it works . 
+    
+                Wether you are a seasoned Gm or a humble noob, we have got you covered. Theres something for everybody. 
+                
+                Get Started : 
+                -> GO to players , Login with  Lichess Account and connect your wallet.
+                -> Click on the verify button and sign the message.Your wallet will now be mapped with lichess account, use it to claim rewards. 
+                -> Participate in games , tournaments on LiChess, submit gameId and claim rewards 
+                -> Buy energy and mint assets based on your play ,trade your assets with other players,make your collection and win.  
+                ----------------------------------------------------------------------------------------------------------------------
+
+            </div>  
+            <div class="flex flex-col gap-y-4">
+              
+                What is Rain ? 
+                Rain Protocol enables you to build web3 economies at scale. Weather it be integrating web3 into an 
+                exsisting ecosystem or building a new one, rain empowers communities and developers to build launchpads, 
+                games, game studios, DAOs or virtual artists, sell land or bootstrap a creative project; either out of 
+                the box or configurable by you, pre-audited, permissionless and available on any EVM compatible chain .
+                Rain is web3 lego. Snap pieces together, build something, then snap new pieces together tomorrow and do something new  
+                ----------------------------------------------------------------------------------------------------------------------
+
+    
+            </div>  
+     
+            
         </TabPanel>
         <TabPanel>
             <LiChessPlayer />
         </TabPanel>
     </Tabs>
-</div>
+</div> 
