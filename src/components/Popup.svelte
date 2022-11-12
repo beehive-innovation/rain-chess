95e241f1220008f1fc4d4453e468057ea6b42f26
SocioDroid <sociodroid2019@gmail.com>
2022-11-12 21:28:46 +0530

    Stepper poc

---
+++ src/components/Popup.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { Steps } from "svelte-steps";
+  let steps = [
+    { text: "[1 XP]" },
+    { text: "[2 XP]" },
+    { text: "[3 XP]" },
+    { text: "[4 XP]" },
+    { text: "[5 XP]" },
+  ];
+
+  export let message = "Default message";
+</script>
+
+<Steps {steps} size="2rem" line="2rem" />
+
+<style>
+  h2 {
+    font-size: 2rem;
+    text-align: center;
+  }
+</style>
